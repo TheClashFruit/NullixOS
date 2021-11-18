122592d0a909cfadb02ca5a80881e5e723aaca18
TheClashFruit <clash.fruit123@gmail.com>
2021-11-18 16:05:23 +0100

    feat: add some stuff

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,23 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+  loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  let vga_buffer = 0xb8000 as *mut u8;
+
+  "Hello, world!"
+    .as_bytes()
+    .iter()
+    .flat_map(|bt| [*bt, 0xf as u8])
+    .enumerate()
+    .for_each(|(i, byte)| unsafe {
+      *vga_buffer.offset(i as isize) = byte;
+    });
+
+  loop {}
 }
